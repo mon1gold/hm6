736ce21f3e4df89751c08bc83e5fda49ae1ba018
Danya <daniildocenko218@gmail.com>
2023-03-11 14:50:38 +0200

    solved the mistakes

--- src/car/Car.java
+++ src/car/Car.java
@@ -2,19 +2,28 @@ package car;
 
 public class Car {
 
-    public void start () {
+    public void start() {
         startElectricity();
         startCommand();
         startFuelSystem();
+        System.out.println("Engine is started");
+
 
     }
+
     private void startElectricity() {
+        System.out.println("Electricity is started");
+
 
     }
+
     private void startCommand() {
+        System.out.println("The launch command is running");
 
     }
+
     private void startFuelSystem() {
+        System.out.println("Fuel system is started");
 
     }
 
